264d5f21e63436fdc6d4b19d865e5ea112ff84b0
Marc Haemmerle <mail@marchaemmerle.com>
2013-09-25 21:58:40 +0200

    a change in style

--- src-cljs/chorf/core.cljs
+++ src-cljs/chorf/core.cljs
@@ -9,37 +9,29 @@
 ;; https://github.com/emezeske/lein-cljsbuild/blob/0.3.3/doc/CROSSOVERS.md
 ;; http://blog.getprismatic.com/blog/2013/4/29/faster-better-dom-manipulation-with-dommy-and-clojurescript
 
-(defn get-elements
-  []
+(defn get-elements []
   (let [result (dx/xpath "//div[contains(concat(' ', normalize-space(@class), ' '), 'fn')]")]
     (map d/text (d/nodes result))))
 
-(defn clear-storage
-  [storage]
+(defn clear-storage [storage]
   (.clear storage))
 
-(defn store-snapshot
-  [storage key elements]
+(defn store-snapshot [storage key elements]
   (.set storage key (JSON/stringify (clj->js {:elements elements}))))
 
-(defn take-snapshot
-  [storage timestamp]
+(defn take-snapshot [storage timestamp]
   (store-snapshot storage (get-elements) timestamp))
 
-(defn storage-keys
-  [storage]
+(defn storage-keys [storage]
   (g-iter/toArray (.__iterator__ storage true)))
 
-(defn storage-values
-  [storage]
+(defn storage-values [storage]
   (g-iter/toArray (.__iterator__ storage false)))
 
-(defn storage-to-map
-  [storage]
+(defn storage-to-map [storage]
   (zipmap (storage-keys storage) (storage-values storage)))
 
-(defn get-diff
-  [storage]
+(defn get-diff [storage]
   (let [sorted-entries (sort (fn [a b] (< (key a) (key b))) (storage-to-map storage))]
     (reduce (fn [acc [k v]]
               (let [elements (set (get (js->clj (JSON/parse v)) "elements"))
@@ -51,22 +43,19 @@
                            nil)]
                 (conj acc {:key k :diff diff :elements elements}))) [] sorted-entries)))
 
-(defn needs-update?
-  [current-timestamp timestamps]
+(defn needs-update? [current-timestamp timestamps]
   (let [sorted-timestamps (reverse (sort timestamps))
         latest-timestamp (first sorted-timestamps)]
     (or (nil? latest-timestamp) (> current-timestamp latest-timestamp))))
 
-(defn local-midnight
-  [date]
+(defn local-midnight [date]
   (let [seconds (+ (* (.getHours date) 3600)
                    (* (.getMinutes date) 60)
                    (.getSeconds date))
         milliseconds (+ (.getMilliseconds date) (* seconds 1000))]
     (Math/round (/ (- (.getTime date) milliseconds) 1000))))
 
-(defn print-summary
-  [data]
+(defn print-summary [data]
   (let [diffs (map
                #(let [diff (:diff %)
                       s (if (nil? diff)
@@ -78,8 +67,7 @@
                   (str (:key %) " -> " s)) data)]
     (console/log (clojure.string/join ",\n" diffs))))
 
-(defn init
-  []
+(defn init []
   (let [storage (goog.storage.mechanism.HTML5LocalStorage.)
         current-timestamp (local-midnight (js/Date.))]
     (when (needs-update? current-timestamp (storage-keys storage))
